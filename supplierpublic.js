08a3d40846b4aab5b9c0e7d6b5838d48f8cb3680
Letícia Lima <leticia8810@gmail.com>
2023-02-24 12:45:03 +0000

    Added write your review form when the write a review button is clicked!

--- supplierpublic.js
+++ supplierpublic.js
@@ -1,24 +1,39 @@
-const reviewForm = document.getElementById('review-form');
-const reviewsList = document.getElementById('reviews-list');
+// const reviewForm = document.getElementById('review-form');
+// const reviewsList = document.getElementById('reviews-list');
 
-reviewForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+// reviewForm.addEventListener('submit', (event) => {
+//   event.preventDefault();
   
-  const name = document.getElementById('name').value;
-  const rating = document.getElementById('rating').value;
-  const comment = document.getElementById('comment').value;
+//   const name = document.getElementById('name').value;
+//   const rating = document.getElementById('rating').value;
+//   const comment = document.getElementById('comment').value;
 
-  const review = document.createElement('div');
-  review.className = 'review';
+//   const review = document.createElement('div');
+//   review.className = 'review';
 
-  const ratingStars = '&#9733;'.repeat(rating);
-  const ratingHtml = `<span class="rating">${ratingStars}</span>`;
+//   const ratingStars = '&#9733;'.repeat(rating);
+//   const ratingHtml = `<span class="rating">${ratingStars}</span>`;
 
-  review.innerHTML = `
-    <p><span class="name">${name}</span> ${ratingHtml} <span class="comment">${comment}</span></p>
-  `;
+//   review.innerHTML = `
+//     <p><span class="name">${name}</span> ${ratingHtml} <span class="comment">${comment}</span></p>
+//   `;
 
-  reviewsList.appendChild(review);
+//   reviewsList.appendChild(review);
 
-  reviewForm.reset();
+//   reviewForm.reset();
+// });
+
+
+/* Reviews submit */
+
+const reviewButton = document.getElementById("review-button");
+const reviewForm = document.getElementById("review-form");
+
+reviewButton.addEventListener("click", function() {
+  if (reviewForm.style.display === "block") {
+    reviewForm.style.display = "none";
+  } else {
+    reviewForm.style.display = "block";
+  }
 });
+
