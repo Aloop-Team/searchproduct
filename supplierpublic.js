c6a7e2cc6f17ca314439246029f475de02556942
Letícia Lima <leticia8810@gmail.com>
2023-02-24 14:43:01 +0000

    Supplier public page - desktop version done

--- supplierpublic.js
+++ supplierpublic.js
@@ -1,39 +1,72 @@
-// const reviewForm = document.getElementById('review-form');
-// const reviewsList = document.getElementById('reviews-list');
-
-// reviewForm.addEventListener('submit', (event) => {
-//   event.preventDefault();
-  
-//   const name = document.getElementById('name').value;
-//   const rating = document.getElementById('rating').value;
-//   const comment = document.getElementById('comment').value;
-
-//   const review = document.createElement('div');
-//   review.className = 'review';
-
-//   const ratingStars = '&#9733;'.repeat(rating);
-//   const ratingHtml = `<span class="rating">${ratingStars}</span>`;
-
-//   review.innerHTML = `
-//     <p><span class="name">${name}</span> ${ratingHtml} <span class="comment">${comment}</span></p>
-//   `;
-
-//   reviewsList.appendChild(review);
-
-//   reviewForm.reset();
-// });
-
-
 /* Reviews submit */
 
-const reviewButton = document.getElementById("review-button");
-const reviewForm = document.getElementById("review-form");
+// var reviewButton = document.getElementById("review-button");
+// var reviewForm = document.getElementById("review-form");
 
-reviewButton.addEventListener("click", function() {
-  if (reviewForm.style.display === "block") {
-    reviewForm.style.display = "none";
-  } else {
+// reviewButton.addEventListener("click", function() {
+//   if (reviewForm.style.display === "block") {
+//     reviewForm.style.display = "none";
+//   } else {
+//     reviewForm.style.display = "block";
+//   }
+// });
+
+/* submitting reviews */
+
+var reviewButton = document.getElementById("review-button");
+var reviewForm = document.getElementById("review-form");
+const customerReviewSubmitted = document.querySelector(".customer-review-submitted");
+
+let isReviewFormVisible = false; // add a variable to keep track of the form visibility
+
+reviewButton.addEventListener("click", () => {
+  if (!isReviewFormVisible) {
     reviewForm.style.display = "block";
+  } else {
+    reviewForm.style.display = "none";
   }
+  isReviewFormVisible = !isReviewFormVisible; // toggle the visibility state
 });
 
+const formWriteReview = document.querySelector(".form-write-review");
+formWriteReview.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  const name = document.getElementById("name").value;
+  const rating = document.getElementById("rating").value;
+  const review = document.getElementById("review").value;
+  
+  const customerReview = document.createElement("div");
+  customerReview.classList.add("customer-review");
+  
+  const h4 = document.createElement("h4");
+  h4.classList.add("customer-review-h4");
+  h4.textContent = name ? name : "Anonymous";
+  customerReview.appendChild(h4);
+  
+  const p1 = document.createElement("p");
+  p1.classList.add("customer-review-p1");
+  for (let i = 0; i < rating; i++) {
+    const starSpan = document.createElement("span");
+    starSpan.classList.add("fa", "fa-star", "checked");
+    p1.appendChild(starSpan);
+  }
+  for (let i = rating; i < 5; i++) {
+    const starSpan = document.createElement("span");
+    starSpan.classList.add("fa", "fa-star");
+    p1.appendChild(starSpan);
+  }
+  customerReview.appendChild(p1);
+  
+  const p2 = document.createElement("p");
+  p2.classList.add("customer-review-p2");
+  p2.textContent = review;
+  customerReview.appendChild(p2);
+  
+  customerReviewSubmitted.insertBefore(customerReview, customerReviewSubmitted.firstChild);
+  
+  formWriteReview.reset();
+  
+  reviewForm.style.display = "none";
+
+});
