042bc9684da492b23f22c9b0525dd7987162bc76
Letícia Lima <leticia8810@gmail.com>
2023-03-09 23:11:12 +0000

    Settings new features

--- team-popup.js
+++ team-popup.js
@@ -1,17 +1,13 @@
 const addContributor = document.querySelector('.addcontributor');
+const editPermission = document.querySelector('.edit-permission');
 const popupOverlay = document.querySelector('.popup-overlay');
 const popupAddContributor = document.querySelector('.popup-addcontributor');
-const editPermission = document.querySelector('.edit-permission');
-
-/* add contributor */
 
 addContributor.addEventListener('click', function(event) {
 event.preventDefault();
 popupOverlay.style.display = 'block';
 popupAddContributor.style.display = 'block';
 });
-
-/* edit contributor */
 
 editPermission.addEventListener('click', function(event) {
 event.preventDefault();
